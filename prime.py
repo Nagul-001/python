43521943182e16d302dc71f27b7ddb44a4d4f508
Nagul-001 <crnagul7@gmail.com>
2025-02-03 12:16:46 +0530

    Program to find prime number

---
+++ prime.py
@@ -0,0 +1,26 @@
+# number=int(input("Enter a number : "))
+
+# def isPrime(number):
+#     for i in range(2,number):
+#         if(number%i==0):
+#             return False
+#     return True
+# # count=isPrime(number)
+# if(isPrime(number)):
+#     print(number, " is a prime number")
+# else:
+#     print(number, " is not a prime number")
+
+# Input number
+# number = 12345
+# reversed_number = 0
+
+# while number > 0:
+#     digit = number % 10
+#     reversed_number = reversed_number * 10 + digit
+#     number = number // 10
+
+# print("Reversed Number:", reversed_number)
+
+li=[1,2,[[10,9]],5]
+print(li[2][0])
